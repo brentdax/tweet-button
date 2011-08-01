63d673fd8c06f88947c6a88aa37ad30e61960204
Brent Royal-Gordon <brent@architechies.com>
2011-08-01 16:43:59 -0700

    Improve nil via tests
    Test not only the internal options_to_query_string method, but also the
    custom_tweet_button and tweet_button methods.

--- test/test_tweet-button.rb
+++ test/test_tweet-button.rb
@@ -1,12 +1,22 @@
 require 'helper'
 
 class TestTweetButton < Test::Unit::TestCase
+  require 'action_view'
+  
+  include ActionView::Helpers::UrlHelper
   include TweetButton
   
-  def test_permits_nil_via
-    assert_equal  "url=http%3A%2F%2Fexample.com&text=Text",
-                  options_to_query_string(:text => "Text", :via => nil, :url => "http://example.com"),
-                  "Generates a proper options string for nil via"
-                  
+  def request
+    Struct.new(:url)["http://example.com"]
+  end
+  
+  def test_permits_nil_options
+    assert_no_match /via/,
+                    options_to_query_string(:text => "Text", :via => nil, :url => "http://example.com"),
+                    "Generates a proper options string for nil via"
+    assert_no_match /via/, custom_tweet_button("Tweet", :via => nil),
+                    "Generates a proper custom tweet button"
+    assert_no_match /data-via/, tweet_button(:via => nil),
+                    "Generates a proper tweet button"
   end
 end
